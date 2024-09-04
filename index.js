8e76938bf7dbb849df722b97205f22e4965ca53d
minnggy23 <rubyshemaria@gmail.com>
2024-09-04 14:09:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+
+const fs = require("fs").promises;
+
+const requestListener = function(req,res){
+    console.log(req.url);
+
+    if(req.url == "/"){
+        fs.readFile(__dirnam + "/page.html")
+            .then(
+                contents => {
+                    res.setHeader("Content-Type", "text/html; charset=UTF-8");
+                    res.writeHeader(200); 
+                    res.end(contents);
+                }
+            )
+    } else{
+        fs.readFile(__dirnam + "/data.json")
+            .then(contents =>{
+                res.setHeader("Content-Type", "application/json; charset=UTF-8");
+                    res.writeHeader(200); 
+                    res.end(contents);
+            });
+    }
+};
+
+const server = http.createSever(requestListener);
+
+const host = "127.0.0.1";
+const port = "3000";
+
+server.listen(
+    port,
+    host,
+    ()=>{
+        console.log('Server is running');
+    }
+);
